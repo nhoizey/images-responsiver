bd07e31a18425d42a6b89f40dec53d5fb6e7ff05
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-13 09:42:57 +0100

    Make tests

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+};
