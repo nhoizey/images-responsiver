3acb9ac9578c56abce510f5994ab0ac2e59bf7f7
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-13 16:39:49 +0100

    Test HTML string transformation is not easy
    Should try with DOM?

--- packages/images-responsiver/index.test.js
+++ packages/images-responsiver/index.test.js
@@ -9,8 +9,21 @@ test('keeps intact HTML without image', () => {
 });
 
 test('HTML with simple image', () => {
-  const content = `<!DOCTYPE html>
-<html><head><title>Hello</title></head><body><p>Hello</p><p><img src="https://example.com/test.png"></p></body></html>`;
+  const content = `<!DOCTYPE html><body>
+<img src="https://example.com/test.png"></body>`;
+  const expectedTransformedContent = `<!DOCTYPE html><body>
+<img
+  src="https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,w_640/https://example.com/test.png"
+  srcset="
+    https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,w_320/https://example.com/test.png 320w,
+    https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,w_880/https://example.com/test.png 880w,
+    https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,w_1440/https://example.com/test.png 1440w,
+    https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,w_2000/https://example.com/test.png 2000w,
+    https://res.cloudinary.com/nho/image/fetch/q_auto,f_auto,w_2560/https://example.com/test.png 2560w"
+    sizes="100vw"
+    data-pristine="https://example.com/test.png"
+    width="null"
+    height="null" /></body>`;
   const transformedContent = imagesResponsiver(content, {});
-  expect(prettier.format(transformedContent, { parser: 'html' })).toEqual(prettier.format(content, { parser: 'html' }));
+  expect(prettier.format(transformedContent, { parser: 'html' })).toEqual(prettier.format(expectedTransformedContent, { parser: 'html' }));
 });
