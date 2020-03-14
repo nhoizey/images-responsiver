0937e32aeea246a52aec7b1c923c3882dc256063
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-14 23:35:33 +0100

    Exit ASAP if the image can't be transformed

--- index.js
+++ index.js
@@ -38,65 +38,69 @@ const imageResponsiver = (html, options) => {
   [...document.querySelectorAll(globalSettings.selector)].forEach(image => {
     const imageSrc = image.getAttribute('src');
 
-    // TODO: use the URL API?
-    if (!imageSrc.match(/^https?:\/\//)) {
-      message(`Image src attribute is not an absolute URL: ${imageSrc}`);
-    } else if (imageSrc.match(/\.svg$/)) {
+    if (imageSrc.match(/\.svg$/)) {
       message(`Nothing to do with SVG: ${imageSrc}`);
+      return;
+    }
+
+    if (!imageSrc.match(/^https?:\/\//)) {
+      // TODO: use the URL API?
+      message(`Image src attribute is not an absolute URL: ${imageSrc}`);
+      return;
+    }
+
+    let imageSettings = globalSettings;
+
+    imageSettings.attributes.width = image.getAttribute('width');
+    if (imageSettings.attributes.width === null) {
+      message(`Images should have a width attribute: ${imageSrc}`);
     } else {
-      let imageSettings = globalSettings;
+      imageSettings.maxWidth = Math.min(imageSettings.maxWidth, imageSettings.attributes.width);
+      imageSettings.fallbackWidth = Math.min(imageSettings.fallbackWidth, imageSettings.attributes.width);
+    }
 
-      imageSettings.attributes.width = image.getAttribute('width');
-      if (imageSettings.attributes.width === null) {
-        message(`Images should have a width attribute: ${imageSrc}`);
-      } else {
-        imageSettings.maxWidth = Math.min(imageSettings.maxWidth, imageSettings.attributes.width);
-        imageSettings.fallbackWidth = Math.min(imageSettings.fallbackWidth, imageSettings.attributes.width);
+    imageSettings.attributes.height = image.getAttribute('height');
+    if (imageSettings.attributes.height === null) {
+      message(`Images should have a height attribute: ${imageSrc}`);
+    }
+
+    // Overhide settings with presets named in the image classes
+    image.classList.forEach(className => {
+      if (options.presets[className] !== undefined) {
+        imageSettings = deepmerge(
+          imageSettings,
+          options.presets[className],
+          { arrayMerge: overwriteMerge }
+        );
       }
+    });
 
-      imageSettings.attributes.height = image.getAttribute('height');
-      if (imageSettings.attributes.height === null) {
-        message(`Images should have a height attribute: ${imageSrc}`);
-      }
+    if (imageSettings.classes.length > 0) {
+      image.classList.add(...imageSettings.classes);
+    }
 
-      // Overhide settings with presets named in the image classes
-      image.classList.forEach(className => {
-        if (options.presets[className] !== undefined) {
-          imageSettings = deepmerge(
-            imageSettings,
-            options.presets[className],
-            { arrayMerge: overwriteMerge }
-          );
-        }
-      });
+    // Change the image source
+    image.setAttribute('src', imageSettings.resizedImageUrl(imageSrc, imageSettings.fallbackWidth));
 
-      if (imageSettings.classes.length > 0) {
-        image.classList.add(...imageSettings.classes);
-      }
+    // generate the srcset attribute
+    let srcset = [];
+    for (let i = 0; i < imageSettings.steps; i++) {
+      let width = Math.ceil(imageSettings.minWidth + (imageSettings.maxWidth - imageSettings.minWidth) / (imageSettings.steps - 1) * i);
+      srcset.push(`${imageSettings.resizedImageUrl(imageSrc, width)} ${width}w`);
+    }
+    image.setAttribute('srcset', srcset.join(', '));
 
-      // Change the image source
-      image.setAttribute('src', imageSettings.resizedImageUrl(imageSrc, imageSettings.fallbackWidth));
+    // add sizes attribute
+    image.setAttribute('sizes', imageSettings.sizes);
 
-      // generate the srcset attribute
-      let srcset = [];
-      for (let i = 0; i < imageSettings.steps; i++) {
-        let width = Math.ceil(imageSettings.minWidth + (imageSettings.maxWidth - imageSettings.minWidth) / (imageSettings.steps - 1) * i);
-        srcset.push(`${imageSettings.resizedImageUrl(imageSrc, width)} ${width}w`);
-      }
-      image.setAttribute('srcset', srcset.join(', '));
+    // add 'data-pristine' attribute with URL of the pristine image
+    image.setAttribute('data-pristine', imageSrc);
 
-      // add sizes attribute
-      image.setAttribute('sizes', imageSettings.sizes);
-
-      // add 'data-pristine' attribute with URL of the pristine image
-      image.setAttribute('data-pristine', imageSrc);
-
-      // Add attributes from the preset
-      if (Object.keys(imageSettings.attributes).length > 0) {
-        for (const attribute in imageSettings.attributes) {
-          if (imageSettings.attributes[attribute] !== null) {
-            image.setAttribute(attribute, imageSettings.attributes[attribute]);
-          }
+    // Add attributes from the preset
+    if (Object.keys(imageSettings.attributes).length > 0) {
+      for (const attribute in imageSettings.attributes) {
+        if (imageSettings.attributes[attribute] !== null) {
+          image.setAttribute(attribute, imageSettings.attributes[attribute]);
         }
       }
     }
