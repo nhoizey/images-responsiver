d0da43a4af87296047d136b0751079bf1f151a9b
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-18 00:31:06 +0100

    Provide an example

---
+++ packages/images-responsiver/examples/01_simple/run.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const imagesResponsiver = require('../../index.js');
+
+const srcHtml = fs.readFileSync('page.html', { encoding: 'utf8' });
+const options = {
+  selector: 'img',
+  resizedImageUrl: (src, width) => `${src}?w=${width}`,
+  presets: {
+    default: {
+      fallbackWidth: 640,
+      minWidth: 320,
+      maxWidth: 1280,
+      sizes: '(max-width: 44em) 90vw, 40em',
+      attributes: {
+        loading: 'lazy'
+      }
+    },
+    logo: {
+      fallbackWidth: 128,
+      minWidth: 58,
+      maxWidth: 256,
+      steps: 3,
+      sizes: '(max-width: 44em) 18vw, 8em',
+      classes: ['logo']
+    },
+    left: {
+      classes: ['left']
+    }
+  }
+};
+const distHtml = imagesResponsiver(srcHtml, options);
+fs.writeFileSync('page-responsive.html', distHtml);
