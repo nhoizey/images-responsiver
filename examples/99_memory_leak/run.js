0ea9bf48fb065e4e456581c1a166604b0518bb5b
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-18 01:30:03 +0100

    Add a bad example for memory issue

---
+++ examples/99_memory_leak/run.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+const imagesResponsiver = require('../../index.js');
+
+const toMB = bytes => Math.floor(bytes / 1024 / 1024);
+
+const srcHtml = fs.readFileSync('./page.html', { encoding: 'utf8' });
+let distHtml;
+let mbUsed;
+
+for (let i = 0; i < 10000; i++) {
+  distHtml = imagesResponsiver(srcHtml, {});
+  if (!(i % 10)) {
+    mbUsed = toMB(process.memoryUsage().heapUsed);
+    console.log(`Tracking memory: ${mbUsed} MB at iteration #${i}`);
+  }
+  // if (mbUsed > 500) {
+  //   global.gc();
+  // }
+}
