1df88b7eab7f0a254ecc8ab905c6a56211d704b3
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-23 22:54:37 +0100

    Simplify first example

---
+++ examples/01_default/run.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const fs = require('fs');
+const imagesResponsiver = require('../../index.js');
+
+const src = fs.readFileSync('./page.html', { encoding: 'utf8' });
+const dist = imagesResponsiver(src);
+fs.writeFileSync('page-responsive.html', dist);
