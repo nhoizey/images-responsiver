9507763edd7a7a22f1b018a76c9081d4bda5ea90
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-25 01:15:36 +0100

    Add comments in scripts

--- packages/images-responsiver/docs/examples/02-images-responsiver-default/run.js
+++ packages/images-responsiver/docs/examples/02-images-responsiver-default/run.js
@@ -1,8 +1,14 @@
 'use strict';
 
+// get dependencies
 const fs = require('fs');
 const imagesResponsiver = require('../../../index.js');
 
+// get the HTML content of the source file
 const src = fs.readFileSync('./page.html', { encoding: 'utf8' });
+
+// run images-responsiver on the source HTML string
 const dist = imagesResponsiver(src);
+
+// write the result into a new file
 fs.writeFileSync('page-enhanced.html', dist);
--- packages/images-responsiver/docs/examples/99_memory_load_test/run.js
+++ packages/images-responsiver/docs/examples/99_memory_load_test/run.js
@@ -1,21 +1,22 @@
 'use strict';
 
+// get dependencies
 const fs = require('fs');
 const imagesResponsiver = require('../../../index.js');
 
 const toMB = (bytes) => Math.floor(bytes / 1024 / 1024);
 
-const srcHtml = fs.readFileSync('./page.html', { encoding: 'utf8' });
-let distHtml;
+// get the HTML content of the source file
+const src = fs.readFileSync('./page.html', { encoding: 'utf8' });
+
+let dist;
 let mbUsed;
 
+// make loops to track memory usage
 for (let i = 0; i < 10000; i++) {
-  distHtml = imagesResponsiver(srcHtml, {});
+  dist = imagesResponsiver(src, {});
   if (!(i % 10)) {
     mbUsed = toMB(process.memoryUsage().heapUsed);
     console.log(`Tracking memory: ${mbUsed} MB at iteration #${i}`);
   }
-  // if (mbUsed > 500) {
-  //   global.gc();
-  // }
 }
