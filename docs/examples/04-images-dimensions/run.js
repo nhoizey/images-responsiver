79411f7cfb0e0ac5d36a20c3060e806d537ace45
Nicolas Hoizey <nicolas@hoizey.com>
2020-03-25 01:15:36 +0100

    Add comments in scripts

--- docs/examples/04-images-dimensions/run.js
+++ docs/examples/04-images-dimensions/run.js
@@ -1,9 +1,13 @@
 'use strict';
 
+// get dependencies
 const fs = require('fs');
 const imagesResponsiver = require('../../../index.js');
 
+// get the HTML content of the source file
 const src = fs.readFileSync('./page.html', { encoding: 'utf8' });
+
+// define images-responsiver options
 const options = {
   presets: {
     default: {
@@ -18,5 +22,9 @@ const options = {
     },
   },
 };
+
+// run images-responsiver on the source HTML string
 const dist = imagesResponsiver(src, options);
+
+// write the result into a new file
 fs.writeFileSync('page-enhanced.html', dist);
