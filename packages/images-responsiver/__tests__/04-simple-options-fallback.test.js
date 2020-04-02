b3f649e508e91928a3760d4f885d74a4a9bc8b77
Nicolas Hoizey <nicolas@hoizey.com>
2020-04-02 02:21:29 +0200

    Add tests and organize them

---
+++ packages/images-responsiver/__tests__/04-simple-options-fallback.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const prettier = require('prettier');
+const imagesResponsiver = require('../index');
+
+function cleanHtml(html) {
+  return prettier.format(html, { parser: 'html' });
+}
+
+describe('image with simple fallback option', () => {
+  test('width attribute superior to the fallback', () => {
+    const content = `<!DOCTYPE html><body>
+      <img src="test.png" width="789"></body>`;
+    const transformed = imagesResponsiver(content, {
+      default: {
+        fallbackWidth: 480,
+      },
+    });
+    expect(cleanHtml(transformed)).toMatchSnapshot();
+  });
+
+  test('width attribute inferior to the fallback', () => {
+    const content = `<!DOCTYPE html><body>
+      <img src="test.png" width="420"></body>`;
+    const transformed = imagesResponsiver(content, {
+      default: {
+        fallbackWidth: 480,
+      },
+    });
+    expect(cleanHtml(transformed)).toMatchSnapshot();
+  });
+});
